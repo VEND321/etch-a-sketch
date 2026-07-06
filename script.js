1aca5c2bed0b6ea6cc189812c86820af6c8bcb8a
VEND321 <victorosunrinde@gmail.com>
2026-07-06 16:14:24 +0100

    feat:can now add number to increase or decrease grid number feat: add loop to check the grid number

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 
 const container = document.querySelector("#container"); 
+const numberOfSqaures = document.querySelector("#number-of-sqaure")
+let squareNumber = 16 * 16;
 
-const squareNumber = 16 * 16;
+//Ask for the number of sqaure input
+numberOfSqaures.addEventListener('click', function(){
+  let userInput = prompt("Enter the number of grid:")
 
-//loops to create the square
-for (let i = 0; i < squareNumber; i++){
+  //convert to integer
+  let sqaurePerSide = parseInt(userInput);
+  let totalSquares = sqaurePerSide* sqaurePerSide;
 
-  const sqaure = document.createElement('div');
+  //clears the container
+  container.innerHTML = "";
 
-  sqaure.classList.add('grid-sqaure');
-  container.appendChild(sqaure)
+  //loops to create the square
+  for (let i = 0; i < squareNumber; i++){
+   const sqaure = document.createElement('div');
+   sqaure.classList.add('grid-sqaure');
+   
+   //update the style size
+   let sizePercent = 100 / sqaurePerSide
+   sqaure.style.flex = `0 0 ${sizePercent}%`
+   sqaure.style.hight = `${sizePercent}%`
 
-  sqaure.addEventListener('mouseover', () => {
-    sqaure.classList.add('active-sqaure');
-  })
+   sqaure.addEventListener('mouseover', () => {
+     sqaure.classList.add('active-sqaure');
+   })
+   container.appendChild(sqaure)
+  }
+})
 
-}
